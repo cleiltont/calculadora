e1ff8ba6993b8db4042219d72b3351923e3e4f12
Cleilton Timoteo <62734990+cleiltont@users.noreply.github.com>
2020-05-17 20:46:24 -0300

    nao terminado

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
-let tela = document.querySelector('input[name=tela');
-let ac = document.querySelector('button[name=limpar]');
-let plefth = document.querySelector('input[name=parentsq]');
-let pright = document.querySelector('input[name=parentdir]');
-let nove = document.querySelector('input[name=nove]');
-let oito = document.querySelector('input[name=oito]');
-let sete = document.querySelector('input[name=sete]');
-let seis = document.querySelector('input[name=seis]');
-let cinco = document.querySelector('input[name=cinco]');
-let quatro = document.querySelector('input[name=quatro]');
-let tres = document.querySelector('input[name=tres]');
-let dois = document.querySelector('input[name=dois]');
-let um = document.querySelector('input[name=um]');
-let zero = document.querySelector('input[name=zero]');
-let divisao = document.querySelector('input[name=div]');
-let subtracao = document.querySelector('input[name=sub]');
-let soma = document.querySelector('input[name=adi]');
-let multiplicacao = document.querySelector('input[name=mult]');
-let resultado = document.querySelector('input[name=resultado]');
-let ponto = document.querySelector('input[name=ponto]');
+// Pega o elemento input com name "tela", do arquivo index
+let tela = document.querySelector('input[name=tela]');
+
+// Variavel do tipo array com os numeros que tem na calculadora
+let numeros = [0,1,2,3,4,5,6,7,8,9];
+
+let operadores = ['+', '-', '*', '/'];
+
+// Pega a tabela do arquivo index.html
+let inputs = document.querySelector('table');
+
+//Esta função encontrar o elemento que você clicar na area
+//da tabela. Tudo que está dentro da tabela.
+inputs.addEventListener('click', function(event){
+
+	//Busca o valor do elemento que foi clicado
+	button = event.target.value;
+	
+	for(numero of numeros){
+		//console.log(typeof numero);
+	}
+
+	//Se o valor for diferente de undefined ou name "resultado",
+	//esse codigo executara.
+	if(button !== undefined){
+		//O que irá mostrar na tela.
+		//Qualquer botão diferente do OK.
+		if(event.target.name !== 'resultado'){
+			//console.log(button);
+			tela.value += button;
+
+			//Transforma em numero
+			num = parseInt(button);
+
+			//Verifica se é um numero
+			let pos = 0;
+			let nums;
+			let somar;
+			if(typeof num === 'number' && isNaN(num) === false){
+				nums = button;
+			}else{
+				if(button.value === "+"){
+					for(num of nums){
+						somar += num;
+						//tela.value = somar;
+						console.log(somar);
+					}
+				}
+			}
+		}
+	}
+});
+
