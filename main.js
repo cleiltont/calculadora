acd187b492e47fb655e0c2c8aa272db89cd0d969
Cleilton Timoteo <62734990+cleiltont@users.noreply.github.com>
2020-05-18 16:36:34 -0300

    Atualização
    Calculadora 1.0.7
    Adicionado tela2.
    Agora os valores são armazenados como array

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 // Pega o elemento input com name "tela", do arquivo index
-let tela = document.querySelector('input[name=tela]');
-
-// Variavel do tipo array com os numeros que tem na calculadora
-let numeros = [0,1,2,3,4,5,6,7,8,9];
-
-let operadores = ['+', '-', '*', '/'];
+let tela1 = document.querySelector('input[name=tela1]');
+let tela2 = document.querySelector('input[name=tela2]');
 
 // Pega a tabela do arquivo index.html
 let inputs = document.querySelector('table');
+
+let numeros = [];
 
 //Esta função encontrar o elemento que você clicar na area
 //da tabela. Tudo que está dentro da tabela.
@@ -15,39 +13,35 @@ inputs.addEventListener('click', function(event){
 
 	//Busca o valor do elemento que foi clicado
 	button = event.target.value;
-	
-	for(numero of numeros){
-		//console.log(typeof numero);
-	}
 
 	//Se o valor for diferente de undefined ou name "resultado",
 	//esse codigo executara.
-	if(button !== undefined){
+	if(button !== undefined && 
+		event.target.type !== 'text' &&
+		event.target.name !== 'ac'){
+		
 		//O que irá mostrar na tela.
 		//Qualquer botão diferente do OK.
-		if(event.target.name !== 'resultado'){
-			//console.log(button);
-			tela.value += button;
+			tela1.value += button;
 
 			//Transforma em numero
 			num = parseInt(button);
 
 			//Verifica se é um numero
-			let pos = 0;
-			let nums;
-			let somar;
 			if(typeof num === 'number' && isNaN(num) === false){
-				nums = button;
-			}else{
-				if(button.value === "+"){
-					for(num of nums){
-						somar += num;
-						//tela.value = somar;
-						console.log(somar);
-					}
-				}
+				numeros.push(num);
+				localStorage.setItem('numero1', numeros);
 			}
-		}
+			if(button.value === "+"){
+				//
+			} else if(button.value === "-"){
+				//
+			}
+
+			tela2.value = localStorage.getItem('numeros');
+		
 	}
+
 });
 
+//localStorage('nums', numeros);
